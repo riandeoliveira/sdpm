b3c3c8965066c3a1756a211b981e2f382784fb94
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-05-21 21:35:28 -0300

    ✨ feat: Create Vanilla init logic

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -11,26 +11,77 @@ export = {
     const { username, technologies, github_username, project_name } = result
 
     const lowerProjectName = project_name.toLowerCase().split(' ').join('-')
-    // const currentYear = new Date().getFullYear()
+    const currentYear = new Date().getFullYear()
 
     switch (technologies) {
       case 'HTML / CSS / JS':
         await generate({
-          template: 'README.md.ejs',
-          target: `${lowerProjectName}/README.md`,
-          props: { project_name, technologies, github_username, username },
+          template: 'vanilla/.github/.gitkeep',
+          target: `${lowerProjectName}/.github/.gitkeep`,
         })
 
         await generate({
-          template: 'index.html.ejs',
+          template: 'vanilla/assets/css/styles.css.ejs',
+          target: `${lowerProjectName}/assets/css/styles.css`,
+        })
+
+        await generate({
+          template: 'vanilla/assets/fonts/.gitkeep',
+          target: `${lowerProjectName}/assets/fonts/.gitkeep`,
+        })
+
+        await generate({
+          template: 'vanilla/assets/icons/.gitkeep',
+          target: `${lowerProjectName}/assets/icons/.gitkeep`,
+        })
+
+        await generate({
+          template: 'vanilla/assets/images/.gitkeep',
+          target: `${lowerProjectName}/assets/images/.gitkeep`,
+        })
+
+        await generate({
+          template: 'vanilla/assets/js/script.js.ejs',
+          target: `${lowerProjectName}/assets/js/script.js`,
+        })
+
+        await generate({
+          template: 'vanilla/assets/videos/.gitkeep',
+          target: `${lowerProjectName}/assets/videos/.gitkeep`,
+        })
+
+        await generate({
+          template: 'vanilla/.gitignore',
+          target: `${lowerProjectName}/.gitignore`,
+        })
+
+        await generate({
+          template: 'vanilla/.index.html.ejs',
           target: `${lowerProjectName}/index.html`,
-          props: { project_name, technologies },
+          props: { project_name },
+        })
+
+        await generate({
+          template: 'vanilla/LICENSE.ejs',
+          target: `${lowerProjectName}/LICENSE`,
+          props: { currentYear, username },
+        })
+
+        await generate({
+          template: 'vanilla/README.md.ejs',
+          target: `${lowerProjectName}/README.md`,
+          props: { project_name, github_username, username },
+        })
+
+        await generate({
+          template: 'vanilla/TODO.md.ejs',
+          target: `${lowerProjectName}/TODO.md`,
         })
 
         break
       case 'React':
         await generate({
-          template: 'README.md.ejs',
+          template: 'vanilla/README.md.ejs',
           target: `${lowerProjectName}/README.md`,
           props: { project_name, technologies, github_username, username },
         })
@@ -38,7 +89,7 @@ export = {
         break
       case 'React + TS':
         await generate({
-          template: 'README.md.ejs',
+          template: 'vanilla/README.md.ejs',
           target: `${lowerProjectName}/README.md`,
           props: { project_name, technologies, github_username, username },
         })
