1974968425df09a49f5b4b9c64fd4b0ff3a5c3dc
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-05-25 14:43:54 -0300

    ✨ feat: Create nodejs with ts option

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -65,6 +65,9 @@ export = {
 
         switch (server_stack) {
           case '⚙️   Node.js':
+            ;(await askByTypeScript())
+              ? createProjectByPath(path)
+              : createProjectByPath()
 
           default:
             break
